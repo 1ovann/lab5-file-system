b1c7958aa6f95526eb5bcc871743855123928727
1ovann <ivanlakocelli@gmail.com>
2023-11-30 21:13:03 +0200

    add docstrings, longest_path fix, tree improve
    1. added docstring to each class and method.
    2. completely changed the get_longest_path method. the previous one had a lot of bugs and worked incorrectly in some conditions, now everything works fine.
    3. replaced argument "indent" by "prefix" and removed multiplication. now it looks much easier

--- lab5.py
+++ lab5.py
@@ -1,39 +1,90 @@
 class File:
-    # pylint: disable=R0903
+    """
+    Represents a file with its name, extension, and size.
+    """
     def __init__(self, name, extension, size):
+        """
+        Initializes a File object.
+
+        Args:
+        - name (str): The name of the file.
+        - extension (str): The extension of the file.
+        - size (int): The size of the file in bytes.
+        """
         self.name = name
         self.extension = extension
         self.size = size
 
     def __str__(self):
+        """
+        Returns a string representation of the File object.
+        """
         return f"{self.name}.{self.extension} ({self.size} bytes)"
 
 
 class Folder:
+    """
+    Represents a folder containing files and subfolders.
+    """
     def __init__(self, name):
+        """
+        Initializes a Folder object.
+
+        Args:
+        - name (str): The name of the folder.
+        """
         self.name = name
         self.files = []
         self.folders = []
 
     def add_file(self, file):
+        """Adds a File object to the current folder."""
         self.files.append(file)
 
     def add_folder(self, folder):
+        """Adds a subfolder (Folder object) to the current folder."""
         self.folders.append(folder)
 
-    def print_contents(self, indent=0):
-        print("  " * indent + self.name + "/")
+    def display_folder_tree(self, prefix=""):
+        """
+        Displays the folder structure.
+
+        Args:
+        - prefix (str): Indentation in the folder tree.
+        """
+        print(prefix + self.name + "/")
         for file in self.files:
-            print("  " * (indent + 1) + str(file))
+            print(prefix + str(file))
         for folder in self.folders:
-            folder.print_contents(indent + 1)
+            folder.display_folder_tree(prefix + "  ")
 
-    def get_longest_path(self):
-        if not self.folders:
-            return [self.name]
+    def get_longest_path(self, path=""):
+        """
+        Retrieves the longest path of files within the folder structure.
 
-        longest_path = max(folder.get_longest_path() for folder in self.folders)
-        return [self.name] + longest_path
+        Args:
+        - path (str): The current path while going through
+         the folder structure.
+
+        Returns:
+        - str: The longest path in the folder structure +
+        the longest named file in this path.
+        """
+        current_path = path + self.name + "/"
+        longest_path = current_path
+
+        for file in self.files:
+            file_path = current_path + file.name
+            if len(file_path) > len(longest_path):
+                longest_path = file_path + "." + file.extension
+                # file view representation (example: file1.mp3)
+
+        for folder in self.folders:
+            folder_path = folder.get_longest_path(current_path)
+            if len(folder_path) > len(longest_path):
+                longest_path = folder_path
+
+        return longest_path
 
 
 def main():
@@ -52,9 +103,9 @@ def main():
     folder1.add_folder(folder2)
     folder2.add_folder(folder3)
 
-    folder1.print_contents()
+    folder1.display_folder_tree()
 
-    print("Найдовший шлях: ", "/".join(folder1.get_longest_path()))
+    print(f"Longest path: {folder1.get_longest_path()}")
 
 
 if __name__ == "__main__":
